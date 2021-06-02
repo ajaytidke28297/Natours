a1477a6aa82d984582c04d96efda38cf1116ba46
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-06-02 22:13:44 +0530

    added review model, route and controller

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ const hpp = require('hpp');
 // 1) Middlewares
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -66,6 +67,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // All other unhandled urls
 app.all('*', (req, res, next) => {
